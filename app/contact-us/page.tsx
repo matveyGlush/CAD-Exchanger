2388dfdcd4f56efa8f11870cd4462c09a0e300a0
Matvey Glushko <matveyglushkospb@gmail.com>
2024-11-05 19:18:50 +0300

    fix styles, add form submition

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,150 @@
+"use client"
+
+import CustomLayout from '@/components/CustomLayout';
+import { Button, TextField, Typography, Paper } from '@mui/material';
+import { Container, styled } from '@mui/system';
+import { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+const FormContainer = styled(Paper)({
+  padding: '2rem',
+  marginTop: '2rem',
+  maxWidth: '400px',
+  margin: 'auto',
+  backgroundColor: '#fff',
+  borderRadius: '8px',
+});
+
+type Inputs = {
+  name: string
+  email: string
+  message: string
+}
+
+export default function ContactPage() {
+
+  const [contactsData, setContactsData] = useState<Inputs | null>(null);
+  const [successMsg, setSuccessMsg] = useState<string | undefined>(undefined);
+  console.log(successMsg)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    setContactsData({ ...data })
+  }
+
+  useEffect(() => {
+    if (!contactsData) return
+    fetch(`https://functions.yandexcloud.net/d4eo4pnc96oovfhiuc6s?name=${contactsData.name}&email=${contactsData.email}&message=${contactsData.message}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSuccessMsg(data.success)
+      })
+      .catch((error) => console.log(error));
+  }, [contactsData]);
+
+  return (
+    <CustomLayout>
+      <Container sx={{
+        padding: '3rem 0 3rem',
+        justifyContent: 'center',
+        textAlign: 'center',
+        mb: 4,
+      }}>
+        <Typography variant="h2" component="h2" sx={{ fontWeight: 'bold', mb: 3, mt: 1 }}>
+          Only CTA on the page
+        </Typography>
+        <FormContainer elevation={3}>
+          {successMsg !== undefined ?
+            <Typography variant="h1">
+              {successMsg}
+            </Typography>
+            :
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <TextField
+                {...register("name", { required: true, maxLength: 40, minLength: 2 })}
+                aria-invalid={errors.name ? "true" : "false"}
+                label="name"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '8px',
+                  },
+                }}
+              />
+              {errors.name?.type === "required" && (
+                <p role="alert">This field is required</p>
+              )}
+              {errors.name?.type === "maxLength" && (
+                <p role="alert">name must be at least 2 symlols</p>
+              )}
+              {errors.name?.type === "minLength" && (
+                <p role="alert">max name length must be less than 40 symlols</p>
+              )}
+
+              <TextField
+                {...register("email", { required: true, maxLength: 40, minLength: 2 })}
+                label="email"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                required
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '8px',
+                  },
+                }}
+              />
+              {errors.email?.type === "required" && (
+                <p role="alert">This field is required</p>
+              )}
+              {errors.email?.type === "maxLength" && (
+                <p role="alert">name must be at least 2 symlols</p>
+              )}
+              {errors.email?.type === "minLength" && (
+                <p role="alert">max name length must be less than 40 symlols</p>
+              )}
+
+              <TextField
+                {...register("message")}
+                label="message"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                multiline
+                rows={4}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '8px',
+                  },
+                }}
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                color="primary"
+                sx={{
+                  marginTop: '1rem',
+                  borderRadius: '8px',
+                  backgroundColor: '#000',
+                  color: '#fff',
+                  '&:hover': { backgroundColor: '#333' },
+                }}
+              >
+                Submit
+              </Button>
+            </form>
+          }
+        </FormContainer>
+      </Container>
+    </CustomLayout>
+  );
+};
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,70 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
+import CustomLayout from '@/components/CustomLayout';
+import { Grid2, Box, Button, Typography, Paper } from '@mui/material';
+import { styled, useMediaQuery } from '@mui/system';
+import Link from 'next/link';
+
+const Section = styled(Box)({
+  padding: '2rem 0',
+  textAlign: 'center',
+});
+
+const ContentPage = () => {
+
+  const matches = useMediaQuery('(max-width:500px)');
+
   return (
-    <main className={styles.main}>
-      <Image
-        className={styles.logo}
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
-      <ol>
-        <li>
-          Get started by editing <code>app/page.tsx</code>.
-        </li>
-        <li>Save and see your changes instantly.</li>
-      </ol>
-
-      <div className={styles.ctas}>
-        <a
-          className={styles.primary}
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            className={styles.logo}
-            src="/vercel.svg"
-            alt="Vercel logomark"
-            width={20}
-            height={20}
-          />
-          Deploy now
-        </a>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.secondary}
-        >
-          Read our docs
-        </a>
-      </div>
-    </main>
+    <CustomLayout>
+      <Section sx={{ mb: 5, backgroundColor: '#f5f7f8', borderRadius: '8px' }}>
+        <Grid2 container spacing={2} justifyContent="center" alignItems="center" sx={{ mt: 4, p: 3 }}>
+          <Grid2 size={{ xs: 12, md: 6 }} sx={{ mb: 3 }}>
+            <Typography variant="h1">
+              Most important title on&nbsp;the page
+            </Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mattis, leo et&nbsp;condimentum ultricies.
+            </Typography>
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 6 }} sx={{}}>
+            {/* <VideoPlaceholder> YouTube video </VideoPlaceholder> */}
+            <iframe width={matches ? 280 : 400} height={matches ? 150 : 250} style={{ borderRadius: '8px' }}
+              src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=btyGVFlMoDfKffwZ"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </Grid2>
+        </Grid2>
+      </Section>
+      <Section sx={{ mb: 5 }}>
+        <Typography variant="h2" component="h2" gutterBottom sx={{ mb: 5 }}>
+          Also very important title
+        </Typography>
+        <Grid2 container spacing={4} justifyContent="center">
+          {[1, 2, 3, 4, 5, 6].map((item) => (
+            <Grid2 size={{ xs: 12, md: 4 }} key={item}>
+              <Paper elevation={0} sx={{ p: '1rem', textAlign: 'center', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                <Typography variant="h3">Title</Typography>
+                <Typography sx={{ maxWidth: 350, textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+              </Paper>
+            </Grid2>
+          ))}
+        </Grid2>
+        <Link href={"contact-us"}>
+          <Button variant="contained" color="primary" sx={{ borderRadius: '8px', mt: 5 }}>Contact&nbsp;us</Button>
+        </Link>
+      </Section>
+      <Section sx={{ backgroundColor: '#f5f7f8', py: '4rem', borderRadius: '8px' }}>
+        <Typography variant="h3">Less important title</Typography>
+        <Link href={"contact-us"}>
+          <Button variant="contained" color="primary" sx={{ borderRadius: '8px', mt: 2 }}>Contact&nbsp;us</Button>
+        </Link>
+      </Section>
+    </CustomLayout>
   );
-}
+};
+
+export default ContentPage;
